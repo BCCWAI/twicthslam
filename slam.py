4c3d1b14efcbc5ec1a79e1e67284b2b434a8b788
bccw <513848839@qq.com>
2018-06-05 17:53:25 +0800

    Essential Matrix pose recovery

--- slam.py
+++ slam.py
@@ -3,19 +3,29 @@ import time
 import cv2
 from display import Display
 from extractor import Extractor
+import numpy as np
 
 W = 1920//2
 H = 1080//2
 
+F = 270
 disp = Display(W, H)
-fe = Extractor()
+K = np.array([[F,0,W//2], [0,F,H//2], [0,0,1]])
+print(K)
+fe = Extractor(K)
 
 def process_frame(img):
   # cv2 + SDL2: 显示绘制纹理多边形
   img = cv2.resize(img,(W, H))
-  matches = fe.extract(img)
+  matches,pose = fe.extract(img)
+  if pose is None:
+    return
 
   print("%d matches" % (len(matches)))
+  print(pose)
+
+  def denormalize(pt):
+    return int(round(pt[0] + img.shape[0]//2)), int(round(pt[1] + img.shape[1]//2))
 
   for pt1, pt2 in matches:
     # 匿名函数lambda关键字 参数x ： 返回值= int(round(x)) 
@@ -25,8 +35,9 @@ def process_frame(img):
     # 第一个参数接受一个函数名，后面的参数接受一个或多个可迭代的序列，
     # 返回的是一个集合。Python函数编程中的map()函数是将func作用于seq中
     # 的每一个元素，并将所有的调用的结果作为一个list返回
-    u1, v1 = map(lambda x: int(round(x)), pt1)
-    u2, v2 = map(lambda x: int(round(x)), pt2)
+    u1, v1 = fe.denormalize(pt1)
+    u2, v2 = fe.denormalize(pt2)
+
     # 要绘制一个圆，需要它的中心坐标和半径
     cv2.circle(img, (u1, v1), color=(0,255,0),radius=3)
     cv2.line(img, (u1, v1), (u2, v2), color=(255,0,0))
